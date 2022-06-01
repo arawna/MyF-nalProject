c7809d815a924f07fc1c106199fa9fce7a962125
ali.hocaoglu <ali.hocaoglu@kocholding.onmicrosoft.com>
2022-06-01 11:28:41 +0300

    01062022-1130

--- DataAccess/Concrete/EntityFramework/EfOrderDal.cs
+++ DataAccess/Concrete/EntityFramework/EfOrderDal.cs
@@ -4,13 +4,12 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCategoryDal : EfEntitiyRepositoryBase<Category, NorthwindContex>, ICategoryDal
+    public class EfOrderDal:EfEntitiyRepositoryBase<Order,NorthwindContex>,IOrderDal
     {
     }
 }
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,7 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,51 +12,17 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntitiyRepositoryBase<Product, NorthwindContex>, IProductDal
     {
-        public void Add(Product entity)
+        public List<ProductDetailDto> GetProductDetails()
         {
             using (NorthwindContex contex = new NorthwindContex())
             {
-                var addedEntity = contex.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                contex.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContex contex = new NorthwindContex())
-            {
-                var deletedEntity = contex.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                contex.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContex contex = new NorthwindContex())
-            {
-                return contex.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContex contex=new NorthwindContex())
-            {
-                return filter == null ? contex.Set<Product>().ToList() : contex.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContex contex = new NorthwindContex())
-            {
-                var updatedEntity = contex.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                contex.SaveChanges();
+                var result = from p in contex.Products
+                             join c in contex.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto { ProductId = p.ProductId, ProductName = p.ProductName, UnitsInStock = p.UnitsInStock, CategoryName = c.CategoryName };
+                return result.ToList();
             }
         }
     }
